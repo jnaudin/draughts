d17a0dfa4abbacb19407fea1b47910351012032d
Johan Naudin <jnaudin@gmail.com>
2021-12-08 16:38:37 +0100

    prevents taking own pieces

--- src/helpers/possibilities.ts
+++ src/helpers/possibilities.ts
@@ -9,41 +9,49 @@ export const getPossibilities = (
   const { line, col } = selectedPiece;
   const pieceToMove = board[line][col];
   const direction = pieceToMove.piece.color === "black" ? 1 : -1;
+  const oppositeColor = pieceToMove.piece.color === "black" ? "white" : "black";
+  const nextMoveLine = line + direction;
 
   const leftMove =
-    col > 0 && !board[line + direction][col - 1].piece
-      ? [{ line: line + direction, col: col - 1 }]
+    nextMoveLine !== -1 &&
+    nextMoveLine !== 10 &&
+    col > 0 &&
+    !board[nextMoveLine][col - 1].piece
+      ? [{ line: nextMoveLine, col: col - 1 }]
       : [];
   const rightMove =
-    col < 9 && !board[line + direction][col + 1].piece
-    ? [{ line: line + direction, col: col + 1 }]
-    : []
+    nextMoveLine !== -1 &&
+    nextMoveLine !== 10 &&
+    col < 9 &&
+    !board[nextMoveLine][col + 1].piece
+      ? [{ line: nextMoveLine, col: col + 1 }]
+      : [];
 
   const topLeftTake =
     col > 1 &&
     line > 1 &&
-    !!board[line - 1][col - 1].piece &&
+    board[line - 1][col - 1]?.piece?.color === oppositeColor &&
     !board[line - 2][col - 2].piece
       ? [{ line: line - 2, col: col - 2 }]
       : [];
   const topRightTake =
     col < 8 &&
     line > 1 &&
-    !!board[line - 1][col + 1].piece &&
+    board[line - 1][col + 1]?.piece?.color === oppositeColor &&
     !board[line - 2][col + 2].piece
       ? [{ line: line - 2, col: col + 2 }]
       : [];
   const bottomLeftTake =
     col > 1 &&
     line < 8 &&
-    !!board[line + 1][col - 1].piece &&
+    board[line + 1][col - 1]?.piece?.color === oppositeColor &&
     !board[line + 2][col - 2].piece
       ? [{ line: line + 2, col: col - 2 }]
       : [];
   const bottomRightTake =
     col < 8 &&
     line < 8 &&
-    !!board[line + 1][col + 1].piece &&
+    board[line + 1][col + 1]?.piece?.color === oppositeColor &&
     !board[line + 2][col + 2].piece
       ? [{ line: line + 2, col: col + 2 }]
       : [];
